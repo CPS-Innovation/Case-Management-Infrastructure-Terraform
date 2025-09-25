3a2737216f37b837ba1928900c7af8462292b07f
Lilach.Davis <Lilach.Davis@cps.gov.uk>
2025-09-25 17:29:15 +0100

    update pipelines

--- environments/dev/base/data.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  resource_group_name = var.vnet_rg
-}
-
-data "azurerm_route_table" "rt" {
-  name                = "rt-${var.project_acronym}-${var.subscription_env}"
-  resource_group_name = var.vnet_rg
-}
