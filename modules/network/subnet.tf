e3b42f09856ee41d7909b2bb283bab9fa42adcf9
Lilach.Davis <Lilach.Davis@cps.gov.uk>
2025-09-25 17:22:28 +0100

    add base layer

---
+++ modules/network/subnet.tf
@@ -0,0 +1,45 @@
+resource "azurerm_subnet" "subnets" {
+  for_each = var.subnets
+
+  name                 = each.key
+  resource_group_name  = var.vnet_rg
+  virtual_network_name = var.vnet_name
+  address_prefixes     = each.value.address_prefixes
+  service_endpoints    = each.value.service_endpoints
+
+  dynamic "delegation" {
+    for_each = each.value.service_delegation == true ? [1] : []
+
+    content {
+      name = "delegation-${each.key}"
+
+      service_delegation {
+        name    = "Microsoft.Web/serverFarms"
+        actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+      }
+    }
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_djb_subnet_assoc" {
+  for_each = var.subnets
+
+  subnet_id                 = azurerm_subnet.subnets[each.key].id
+  network_security_group_id = var.create_nsg ? azurerm_network_security_group.nsg[0].id : var.nsg_id
+
+  depends_on = [
+    azurerm_subnet.subnets,
+  ]
+}
+
+resource "azurerm_subnet_route_table_association" "djb_rt" {
+  for_each = var.subnets
+
+  subnet_id      = azurerm_subnet.subnets[each.key].id
+  route_table_id = var.route_table_id
+
+  depends_on = [
+    azurerm_subnet.subnets,
+    azurerm_network_security_group.nsg
+  ]
+}
