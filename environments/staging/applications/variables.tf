f4298e4bd58cfd15127de240d27b156cb5d2e52a
Lilach.Davis <Lilach.Davis@cps.gov.uk>
2025-10-08 12:20:43 +0100

    add staging env app layer

---
+++ environments/staging/applications/variables.tf
@@ -0,0 +1,24 @@
+variable "project_acronym" {
+  type        = string
+  description = "The abbreviated project name."
+}
+
+variable "environment" {
+  type        = string
+  description = "The deployment environment."
+}
+
+variable "location" {
+  type        = string
+  description = "Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created"
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "The name of the virtual network in which to create the subnet"
+}
+
+variable "vnet_rg" {
+  type        = string
+  description = "The name of the virtual network in which to create the subnet"
+}
