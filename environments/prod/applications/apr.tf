85d4d2adffa809d867321660ad2b71043da1a813
Lilach <Lilach.Davis@cps.gov.uk>
2026-09-02 16:44:09 +0100

    update api exceptions log query alert. Add alert processing rule

---
+++ environments/prod/applications/apr.tf
@@ -0,0 +1,12 @@
+module "alert_processing_rule" {
+  source = "../../../modules/alert-processing"
+
+  environment     = var.environment
+  project_acronym = var.project_acronym
+  tags            = local.tags
+  rg_name         = module.rg.rg_name
+
+  suppression_scopes = [
+    module.rg.rg_id
+  ]
+}
